758a090aae7bfc3f3f31b742295de9f0b9da313c
YilunAllenChen <allenchen@gatech.edu>
2019-05-24 04:47:07 +0800

    wired mongoDB with chart.js

--- app.js
+++ app.js
@@ -2,7 +2,6 @@
 const fs = require('fs');
 const express = require('express');
 var bodyParser = require("body-parser");
-var DP = require("./modules/dataPost");  //data point prototype
 
 //setting up app and config.
 const hostname = '127.0.0.1';
@@ -26,21 +25,10 @@ MongoClient.connect('mongodb://localhost:27017', function (err, client) {
 //routings
 app.get('/', (req, res) => {   //basic routing
   fs.readFile('randomData.json', 'utf8', function (err, contents) {
-    res.send('Home page' + contents);
+    res.send('Home page');
   });
   console.log("called readFile");
 });
-
-// app.get('/data', (req, res) => {
-//   var co = req.query;  //receive the query. http req: in form link/?key=value&key=value&key=value.
-//   console.log(co);
-//   coll.insertOne(co);
-//   coll.find({ id: '1301' }).toArray((err, items) => {
-//     console.log(items)
-//   })
-//   console.log("called readFile");
-// });
-
 
 app.post('/dataPost', (req, res) => {
 
@@ -75,11 +63,23 @@ app.post('/dataPost', (req, res) => {
 
 
 app.get('/visitor', (req, res) => {    //example of opening up another file under the same dir.
-  res.sendFile('/visitorPage.html', { root: __dirname });  //file path must be absolute.
-  coll.find({}).toArray(function (err, items) {
-    console.log(items);
+  //res.sendFile('./modules/chart.html', { root: __dirname });  //file path must be absolute.
+  coll.find({ signature : 1}).toArray(function (err, items) {
+    fs.readFile('./modules/chart.html', 'utf-8', function (err, data) {
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      var chartData = [];
+      items.forEach(element => {
+        chartData.push(element.dataValue);
+        console.log(element);
+      });
+      var result = data.replace('{{chartData1}}', JSON.stringify(chartData));
+      res.write(result);
+      res.end();
+    });
+
   });
 });
+
 
 app.get('/clearData', (req, res) => {
   coll.remove();
