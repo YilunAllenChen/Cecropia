63174213e8886de88daa6ec3986d8fc112339d83
YilunAllenChen <allenchen@gatech.edu>
2019-05-30 00:00:55 +0800

    added the data point count feature

--- app.js
+++ app.js
@@ -87,6 +87,10 @@ app.get('/visitor', (req, res) => {
 
       //change the title of the chart.
       var result = result.replace('{{ dataType }}', req.query.dataType);
+
+      //change the data points count.
+      var result = result.replace('{{ Data Point Count }}', sampleParser.dataCount);
+
       res.write(result);
       res.end();
     });
