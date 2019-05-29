be629ed966b25bd583b56166f896522b50c13dfb
YilunAllenChen <allenchen@gatech.edu>
2019-05-30 01:43:13 +0800

    added moment.js for timestamp

--- modules/dataParser.js
+++ modules/dataParser.js
@@ -1,24 +1,41 @@
 function dataParser(items, numOfAgents) {
-    let colors = ['#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6', 
-    '#E6B333', '#3366E6', '#999966', '#99FF99', '#B34D4D',
-    '#80B300', '#809900', '#E6B3B3'];
+    let colors = ['#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6',
+        '#E6B333', '#3366E6', '#999966', '#99FF99', '#B34D4D',
+        '#80B300', '#809900', '#E6B3B3'];
     let chartData = []; //all the datas.
     let replacement = ''; //the replacement string to be put in place of datasets.
     let dataPointsCountByAgents = [];
     for (let ndx = 0; ndx < numOfAgents; ndx++) {
-        let agentData = [];
+        let agentData = '{data: [';
         chartData[ndx] = [];
+        let dataCount = 0;
+        let isEmpty = true;
         items.forEach(element => {
             let signature = Object.values(element)[1];
-            if(signature == ndx){
-                agentData.push(element.dataValue);
+            if (signature == ndx) {
+                let timeStamp = "\"" + Object.values(element)[2] + "\"";
+                let dataPoint = "{x: " + timeStamp + ", y: " + element.dataValue + "},";
+                agentData = agentData + dataPoint;
+                dataCount = dataCount + 1;
+                isEmpty = false;
             }
-        })
+            
+        });
+        if(!isEmpty){
+            agentData = agentData.substr(0,agentData.length-1)
+        }
+        agentData = agentData + "],\nlabel: 'Agent " + ndx + "',\nfill: false, \nborderColor: '" + colors[ndx] + "'\n}";
         chartData[ndx] = agentData;
-        dataPointsCountByAgents.push(agentData.length);
-        let someSet = new dataSet(chartData[ndx],'agent ' + ndx,colors[ndx]);
-        replacement = replacement + JSON.stringify(someSet);
-        if(ndx != numOfAgents-1){
+
+        dataPointsCountByAgents.push(dataCount);
+
+
+        // let someSet = new dataSet(chartData[ndx], 'agent ' + ndx, colors[ndx]);
+        // replacement = replacement + JSON.stringify(someSet);
+
+        replacement = replacement + agentData;
+        console.log(replacement);
+        if (ndx != numOfAgents - 1) {
             replacement = replacement + ',\n';
         }
     }
@@ -28,7 +45,7 @@ function dataParser(items, numOfAgents) {
     this.dataCount = items.length;
 }
 
-function dataSet(data, label, color){
+function dataSet(data, label, color) {
     this.data = data;
     this.label = label;
     this.borderColor = color;
