9025ffc4c333c6530218fadbc5a3a4b752ed3c40
YilunAllenChen <allenchen@gatech.edu>
2019-05-31 15:30:26 -0400

    improved mock data generator : auto posting

--- RDP.py
+++ RDP.py
@@ -1,7 +1,7 @@
 import random as rd
 import requests
 
-dataTypes = ["temperature", "humidity", "vibration"]
+dataTypes = ['temperature', 'humidity', 'vibration']
 acceptables = ['00', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12',
                '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27',
                '28', '29', '30']
@@ -9,28 +9,25 @@ acceptables = ['00', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10',
 dataValueCache = 50
 
 
-
-for month in acceptables[1:6]:
-    f = open("randomDataPost.json", "w")
-    dataSet = '['
+for month in acceptables[4:6]:
     for day in acceptables[1:31]:
-        
+        dataSet = '['
         for hour in acceptables[0:24:6]:
             for agent in range(10):
                 dataPoint = {
-                    "signature": agent,
-                    "timeStamp": int('2019' + month + day + hour),
-                    "dataType": dataTypes[1],
-                    "dataValue": dataValueCache
+                    'signature': agent,
+                    'timeStamp': int('2019' + month + day + hour),
+                    'dataType': dataTypes[1],
+                    'dataValue': dataValueCache
                 }
                 if(dataValueCache > 100):
-                    dataValueCache = dataValueCache + rd.randint(-3,1)
+                    dataValueCache = dataValueCache + rd.randint(-3, 1)
                 elif(dataValueCache < 0):
-                    dataValueCache = dataValueCache + rd.randint(-1,3)
+                    dataValueCache = dataValueCache + rd.randint(-1, 3)
                 else:
-                    dataValueCache = dataValueCache + rd.randint(-2,2)
+                    dataValueCache = dataValueCache + rd.randint(-2, 2)
                 dataSet = dataSet + str(dataPoint) + ',\n'
-    dataSet = dataSet[0:-2] + "]"
-    dataSet = dataSet.replace('\'','\"')
-    f.write(dataSet)
-f.close()
+        dataSet = dataSet[0:-2] + ']'
+        r = requests.post('http://localhost/dataPost', data=dataSet)
+        pastebin_url = r.text 
+        print('date: ' + month + day + " : " + pastebin_url)
